70d27d50ff085f8afd4dcdbb0cb0fb43ca03e083
corneliuhoffman <corneliuhoffman@gmail.com>
2026-07-14 15:25:57 +0100

    urme annotate: log a milestone line every ~100 functions

--- bin/main.ml
+++ bin/main.ml
@@ -309,9 +309,11 @@ let annotate_cmd =
       let* pool = Urme_claude.Prompts.spawn_pool
           ~model ~size:(max 1 parallel)
           ~system_prompt:annotate_system ~binary:config.claude_binary () in
+      let last = ref min_int in
       let rec loop () =
         let (total, described, _) = Cg.status db in
-        Printf.printf "\rannotating: %d/%d%!" described total;
+        if described - !last >= 100 || described >= total then
+          (Printf.printf "annotated %d/%d\n%!" described total; last := described);
         if described >= total then Lwt.return_unit
         else begin
           let units = Cg.next_ready_file_units db ~limit:(max 1 parallel) in
